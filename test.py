2f12538386a501e9fc839f52a71aa0c77d09b2f6
tdcoleman127 <figgeryfirehead@gmail.com>
2025-09-06 18:05:15 -0500

    Current not working code for building Huffman Code Tree with heapq, switch to PriorityQueue() class soon

--- test.py
+++ test.py
@@ -5,6 +5,19 @@ Created on Mon Sep  1 18:27:24 2025
 @author: troy
 """
 
+import heapq
+import os
+
+# Node class for Huffman Tree
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+    
+    def __lt__(self, other):
+        return self.key < other.key
+
 # functions for Step 1
 def getFrequencyCounts(fname):
     myDict = {}
@@ -16,10 +29,44 @@ def getFrequencyCounts(fname):
             else:
                 myDict[char] += 1
     file.close()
+    print("The dictionary of frequency counts for " + fname + " is: ")
     print(myDict)
     return myDict
 
 def buildHuffmanCodeTree(freqCounts):
+    # Get the dictionary of frequency counts
+    # And initialize priority queue (MUST heapify first!)
+    priority_queue = []
+    heapq.heapify(priority_queue)
+
+    #Initalizing priority queue
+
+    for c in freqCounts:
+        print("Initializing node: " + c)
+        print("Character frequency value: ")
+        print(freqCounts[c])
+        T = Node(freqCounts[c])
+        heapq.heappush(priority_queue, T)
+    
+    while len(priority_queue) > 1:
+        print(priority_queue)
+        f1 = heapq.nsmallest(1, priority_queue)
+        t1 = heapq.heappop(priority_queue)
+        f2 = heapq.nsmallest(1, priority_queue)
+        t2 = heapq.heappop(priority_queue)
+
+        # Creating the linking node
+        print("Creating linking node")
+        print("Linking node key")
+        T = Node(f1.key + f2.key)
+        T.left = t1
+        T.right = t2
+
+        # Pushing the linking node to the next iteration of the min heap
+        heapq.heappush(priority_queue, T)
+
+    # print(priority_queue)
+    return heapq.heappop(priority_queue)
     pass
 
 def getHuffmanCodes(huffTree):
@@ -40,7 +87,8 @@ def convertFromHuffman(encodedname, hcodename):
 def cs351Proj1():
     fname = input("Enter filename")
     print ("Original File name:", fname)
-    getFrequencyCounts(fname)
+    freqDict = getFrequencyCounts(fname)
+    buildHuffmanCodeTree(freqDict)
     
     hcodename = determineHuffmanCode(fname)
     print ("Huffman Code File name:", hcodename)
