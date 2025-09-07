18a1103b07d1f186c6699d6349c8ab2443ce1add
tdcoleman127 <figgeryfirehead@gmail.com>
2025-09-07 16:34:53 -0500

    Got a beta test version of part 2 working in a wordy2.py file

--- wordy2.py
+++ wordy2.py
@@ -1,10 +1,10 @@
 testDict = {}
-hcodename = "runner-hcp.txt"
+hcodename = "newone-hcp.txt"
 encodedStr = ""
 
 # Opening up the -hcp.txt file to read
-file = open(hcodename, "r")
-for line in file:
+file1 = open(hcodename, "r")
+for line in file1:
     print(line)
     # Split by four spaces in file
     boxed = line.split("    ")
@@ -15,14 +15,32 @@ for line in file:
     print("New word: " + newWord)
     # Adding to a created dictionary of ASCII keys and Huffman Code values
     testDict[chr(int(boxed[0]))] = boxed[1].rstrip('\n')
-    encodedStr += boxed[1].rstrip('\n')
-file.close()
+    # encodedStr += boxed[1].rstrip('\n')
+file1.close()
 
+# Verifying results
 print(testDict)
 print("Encoded string: " + encodedStr)
 
+# Do for the original file
+fname = "newone.txt"
+encodedStr2 = ""
+file2 = open(fname, "r")
+for line in file2:
+    for char in line:
+        if char in testDict:
+            # print(char)
+            print(char + " was found")
+            huffVal = testDict[char]
+            encodedStr2 += huffVal
+print(encodedStr2)
 
-# boxed = fname.split("    ")
-# newName = boxed[0] + "-hcp." + boxed[1]
-# # Create a ke
-# pairDict[boxed[0]] = boxed[1]
+
+# Creating new filename with "-hec"
+boxed = ""
+boxed = fname.split(".")
+newName = boxed[0] + "-hec." + boxed[1]
+# Writing to new file
+new_file = open(newName, "w")
+new_file.write(encodedStr2)
+new_file.close()
